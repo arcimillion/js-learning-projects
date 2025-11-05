b400981f48c9517c330719ed946d623f466c68ca
arcimillion <harshvardhansikarwar1412@gmail.com>
2025-11-05 13:07:39 +0530

    local storage

--- bettertodo/btodo.js
+++ bettertodo/btodo.js
@@ -1,19 +1,63 @@
-const b=document.querySelector('button');
-const i=document.querySelector('input');
-b.addEventListener('click', add);
-function add(){
-    let l=document.createElement('li');
-    l.innerText=i.value;
-    const d=document.createElement('button');
-    d.innerText='X';
-    d.style.backgroundColor='red';
-    d.style.color='white';
-    d.addEventListener('click',rem)
-    l.append(d);
-    document.querySelector('ul').appendChild(l);
-    i.value='';
+const b = document.querySelector('button');
+const i = document.querySelector('input');
+const ul = document.querySelector('ul');
 
-    function rem(event){
-        this.parentElement.remove();
+loadTodos();
+
+b.addEventListener('click', add);
+
+function add() {
+    if (i.value === '') {
+        return;
     }
+
+    let l = document.createElement('li');
+    l.innerText = i.value;
+
+    const d = document.createElement('button');
+    d.innerText = 'X';
+    d.style.backgroundColor = 'red';
+    d.style.color = 'white';
+    d.addEventListener('click', rem);
+
+    l.append(d);
+    ul.appendChild(l);
+    i.value = '';
+
+    saveTodos();
+}
+
+function rem(event) {
+    this.parentElement.remove();
+    saveTodos();
+}
+
+function saveTodos() {
+    const todos = [];
+    const listItems = document.querySelectorAll('ul li');
+    
+    listItems.forEach(function(item) {
+        const itemText = item.firstChild.textContent;
+        todos.push(itemText);
+    });
+
+    localStorage.setItem('myTodos', JSON.stringify(todos));
+}
+
+function loadTodos() {
+    const savedTodos = JSON.parse(localStorage.getItem('myTodos') || '[]');
+
+    savedTodos.forEach(function(todoText) {
+        let l = document.createElement('li');
+        l.innerText = todoText;
+
+        const d = document.createElement('button');
+        d.innerText = 'X';
+        d.style.backgroundColor = 'red';
+        d.style.color = 'white';
+        d.addEventListener('click', rem);
+
+        l.append(d);
+        ul.appendChild(l);
+    });
 }
